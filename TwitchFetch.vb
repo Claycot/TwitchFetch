da97b9a65e4f07fe849980082ea5db33898c8e5f
Clay <Claycot@users.noreply.github.com>
2015-01-27 18:19:43 -0500

    Update TwitchFetch.vb
    404 handling added!

--- TwitchFetch.vb
+++ TwitchFetch.vb
@@ -18,19 +18,19 @@ Public Class TwitchFetch
 
     'Function to retrieve text from input url.
     Public Function LoadSiteContent(ByVal url As String) As String
-        'Create a new WebClient object
+        'Create a new WebClient object.
         Dim client As New System.Net.WebClient()
-        'Create a byte array for holding the returned data
+        'Create a byte array for holding the returned data.
         Dim html As Byte() = client.DownloadData(url)
-        'Use the UTF8Encoding object to convert the byte
-        'Array into a string
+        'Use the UTF8Encoding object to convert the byte array into a string.
         Dim utf As New System.Text.UTF8Encoding()
-        'Return the converted string
+        'Return the converted string.
         Return utf.GetString(html)
     End Function
 
     'Large button.
     Private Sub btnfetch_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnfetch.Click
+        m3uraw = "Empty"
         rawinput = txtinput.Text
         channelname = rawinput.Trim.ToLower
         randomnum = CInt(Math.Floor((999999 - 1 + 1) * Rnd())) + 1
@@ -44,44 +44,54 @@ Public Class TwitchFetch
             End If
         Next
 
-        'Grabs m3u8 as string using the required variables.
-        m3uraw = LoadSiteContent("http://usher.twitch.tv/api/channel/hls/" & channelname & ".m3u8?player=twitchweb&token=" & token & "&sig=" & sig & "&$allow_audio_only=true&allow_source=true&type=any&p=" & randomnum)
-
-        'Parses m3uraw for all occurrances of "http://".
-        Dim m3uparse As New System.Text.RegularExpressions.Regex("http://.*")
-        Dim m3umatch As System.Text.RegularExpressions.MatchCollection = m3uparse.Matches(m3uraw)
-
-        'For each parsed link, a variable is assigned based on quality.
-        For Each link As System.Text.RegularExpressions.Match In m3umatch
-            If link.Value.Contains("chunked") Then
-                outputsource = link.Value
-            ElseIf link.Value.Contains("high") Then
-                outputhigh = link.Value
-            ElseIf link.Value.Contains("medium") Then
-                outputmedium = link.Value
-            ElseIf link.Value.Contains("low") Then
-                outputlow = link.Value
-            ElseIf link.Value.Contains("mobile") Then
-                outputmobile = link.Value
+        'Grabs m3u8 as string using the required variables. If the request fails, a 404 message is returned.
+        Try
+            m3uraw = LoadSiteContent("http://usher.twitch.tv/api/channel/hls/" & channelname & ".m3u8?player=twitchweb&token=" & token & "&sig=" & sig & "&$allow_audio_only=true&allow_source=true&type=any&p=" & randomnum)
+        Catch ex As System.Net.WebException
+            If ex.Status = Net.WebExceptionStatus.ProtocolError Then
+                rtfout.Text = "404. Stream is either offline or nonexistent."
             End If
-        Next
+        End Try
 
-        'Depending on what quality is checked, the link is outputted into the rtf box.
-        If rdqualsrc.Checked = True Then
-            rtfout.Text = outputsource
-        ElseIf rdqualhigh.Checked = True Then
-            rtfout.Text = outputhigh
-        ElseIf rdqualmed.Checked = True Then
-            rtfout.Text = outputmedium
-        ElseIf rdquallow.Checked = True Then
-            rtfout.Text = outputlow
-        Else
-            rtfout.Text = "Please select a quality option."
-        End If
+        'If request succeeds, the following occurs.
+        If m3uraw <> "Empty" Then
 
-        'Opens chat in browser if checked.
-        If checkchat.Checked = True Then
-            Process.Start("http://www.twitch.tv/" & channelname & "/chat")
+            'Parses m3uraw for all occurrances of "http://".
+            Dim m3uparse As New System.Text.RegularExpressions.Regex("http://.*")
+            Dim m3umatch As System.Text.RegularExpressions.MatchCollection = m3uparse.Matches(m3uraw)
+
+            'For each parsed link, a variable is assigned based on quality.
+            For Each link As System.Text.RegularExpressions.Match In m3umatch
+                If link.Value.Contains("chunked") Then
+                    outputsource = link.Value
+                ElseIf link.Value.Contains("high") Then
+                    outputhigh = link.Value
+                ElseIf link.Value.Contains("medium") Then
+                    outputmedium = link.Value
+                ElseIf link.Value.Contains("low") Then
+                    outputlow = link.Value
+                ElseIf link.Value.Contains("mobile") Then
+                    outputmobile = link.Value
+                End If
+            Next
+
+            'Depending on what quality is checked, the link is outputted into the rtf box.
+            If rdqualsrc.Checked = True Then
+                rtfout.Text = outputsource
+            ElseIf rdqualhigh.Checked = True Then
+                rtfout.Text = outputhigh
+            ElseIf rdqualmed.Checked = True Then
+                rtfout.Text = outputmedium
+            ElseIf rdquallow.Checked = True Then
+                rtfout.Text = outputlow
+            Else
+                rtfout.Text = "Please select a quality option."
+            End If
+
+            'Opens chat in browser if checked.
+            If checkchat.Checked = True Then
+                Process.Start("http://www.twitch.tv/" & channelname & "/chat")
+            End If
         End If
     End Sub
 End Class
